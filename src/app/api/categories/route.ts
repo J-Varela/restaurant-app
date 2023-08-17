3a5af4d671629fe4c1a48cb5ca3dc9c99a7e140a
Jonathan Varela <jonvarela4@gmail.com>
2023-08-17 01:01:08 -0400

    modified api and menu page

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,15 +1,13 @@
 import { NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { prisma } from '@/utils/connect';
 
 // FETCH ALL CATEGORIES
 export const GET = async () => {
   try {
     const categories = await prisma.category.findMany();
-    return new NextResponse(JSON.stringify(categories), { status: 500 });
-  } catch (err) {
-    console.log(err);
+    return new NextResponse(JSON.stringify(categories), { status: 200 });
+  } catch (error) {
+    console.log(error);
     return new NextResponse(
       JSON.stringify({ message: 'Something went wrong!' }),
       { status: 500 }
